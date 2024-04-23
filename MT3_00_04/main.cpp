cb6cacf50c352714edc7e29fc612658ba088631c
Mituwow <114716282+Mituwow@users.noreply.github.com>
2024-04-23 17:03:52 +0900

    00-04コード記述。課題完了。

---
+++ MT3_00_04/main.cpp
@@ -0,0 +1,161 @@
+#include <Novice.h>
+#include <cmath>
+
+const char kWindowTitle[] = "GC2A_09_ハラサワミツタカ";
+const int kWindowWidth = 1280;
+const int kWindowHeight = 720;
+
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+Matrix4x4 Multiply(const Matrix4x4& a, const Matrix4x4& b) {
+	Matrix4x4 result;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			result.m[i][j] = a.m[i][0] * b.m[0][j] + a.m[i][1] * b.m[1][j] + a.m[i][2] * b.m[2][j] + a.m[i][3] * b.m[3][j];
+		}
+	}
+	return result;
+}
+Matrix4x4 MakeRotateXMatrix4x4(float radian) {
+	Matrix4x4 result;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			result.m[i][j] = 0;
+		}
+	}
+	result.m[0][0] = 1;
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);;
+	result.m[3][3] = 1;
+
+	return result;
+}
+Matrix4x4 MakeRotateYMatrix4x4(float radian) {
+	Matrix4x4 result;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			result.m[i][j] = 0;
+		}
+	}
+	result.m[0][0] = std::cos(radian);
+	result.m[0][2] = -std::sin(radian);
+	result.m[1][1] = 1;
+	result.m[2][0] = std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	result.m[3][3] = 1;
+	return result;
+}
+Matrix4x4 MakeRotateZMatrix4x4(float radian) {
+	Matrix4x4 result;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			result.m[i][j] = 0;
+		}
+	}
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	result.m[2][2] = 1;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+	for (int row = 0; row < 4; row++)
+	{
+		for (int column = 0; column < 4; column++)
+		{
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight,
+				"%6.02f", matrix.m[row][column]
+			);
+		}
+	}
+}
+
+
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	//宣言開始
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix4x4(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix4x4(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix4x4(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
+
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		MatrixScreenPrintf(0, 0, rotateXMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix);
+
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
